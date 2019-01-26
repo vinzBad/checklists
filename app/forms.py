aace50469225f6f947c4738c73c87edd8a03d26a
Vinzenz Sinapius <vinzenz.sinapius@gmail.com>
2019-01-26 18:34:03 +0100

    add css and abilty to add items

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FormField, FieldList, SelectField
+from wtforms import HiddenField,IntegerField, StringField, FormField, FieldList, SelectField
 from wtforms.validators import DataRequired
 from .models import ChecklistItemTypes
 
+
    
 class ChecklistTemplateItemForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    description = StringField("description")
-    itemtype = SelectField("itemtype", choices=ChecklistItemTypes.choices(), coerce=ChecklistItemTypes)
+    id = HiddenField()
+    name = StringField('Name:', [DataRequired()])
+    description = StringField("Description: ")
+    itemtype = SelectField("Typ:", choices=ChecklistItemTypes.choices(), coerce=ChecklistItemTypes)
 
 
 class ChecklistTemplateForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    description = StringField("description")
+    id = HiddenField()
+    name = StringField('Name:', [DataRequired()])
+    description = StringField("Description:")
     items = FieldList(FormField(ChecklistTemplateItemForm))
--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ def index():
     templates = ChecklistTemplate.query.all()
     return render_template("index.html", templates=templates)
 
-@bp.route("/template/<int:template_id>/edit", methods=["POST", "GET"])
+@bp.route("/template/edit/<int:template_id>", methods=["POST", "GET"])
 def edit_template(template_id):
     template = ChecklistTemplate.query.get_or_404(template_id)
     form = ChecklistTemplateForm(request.form, obj=template)
@@ -25,3 +25,16 @@ def edit_template(template_id):
         db.session.add(template)
         db.session.commit()
     return render_template("edit_template.html", template=template, form=form)
+
+@bp.route("/template/add_item/<int:template_id>", methods=["POST"])
+def add_template_item(template_id):
+    template = ChecklistTemplate.query.get_or_404(template_id)
+    item = ChecklistTemplateItem()
+    item.name = "New Item"
+    template.items.append(item)
+    db.session.add(template)
+    db.session.add(item)
+    db.session.commit()
+    
+    
+    return redirect(url_for("views.edit_template", template_id=template_id))
